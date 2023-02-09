d298c6a9f19d35da49ddfe6c80173ebc7deed223
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-09 16:33:59 +0100

    Refactor movies info as cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.app')
 
 @section('main-content')
-    @foreach ($movies as $film)
-        <div class="card">
-            <h1>{{ $film->title}}</h1>
-            <p>Titolo originale {{ $film->original_title }}</p>
-            <p>Origine {{ $film->nationality }}</p>
-            <p>Disponibile dal {{ $film->date }}</p>
-            <p>Voto {{ $film->date }}</p>
+<main>
+    <div class="container mt-5">
+        <div class="row">
+            @foreach ($movies as $film)
+            <div class="col-4 mb-4">     
+                <div class="card p-3">
+                    <h2 class="title text-center">{{ $film->title }}</h2>
+                    <p>Titolo originale: <span>{{ $film->original_title }}</span></p>
+                    <p>Origine: <span>{{ $film->nationality }}</span></p>
+                    <p>Disponibile dal: <span>{{ $film->date }}</span></p>
+                    <p>Voto: <span>{{ $film->vote }}</span></p>
+                </div>
+            </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
+    
+        
+    
+
+</main>
 @endsection
