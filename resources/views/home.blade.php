63d568aca67bbe8e29a0226395e7f5238ce5eb10
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-09 15:59:07 +0100

    Printing movies db infos to the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('main-content')
-    Movies
+    @foreach ($movies as $film)
+        <div class="card">
+            <h1>{{ $film->title}}</h1>
+            <p>Titolo originale {{ $film->original_title }}</p>
+            <p>Origine {{ $film->nationality }}</p>
+            <p>Disponibile dal {{ $film->date }}</p>
+            <p>Voto {{ $film->date }}</p>
+        </div>
+    @endforeach
 @endsection
